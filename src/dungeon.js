99e5dedf31b7aac8c496336bc2f24da7fb956e9d
Magic-Mayo <mike.mayoii@gmail.com>
2020-08-19 04:31:14 -0700

    added in dungeon checker function. if it returns false the dungeon created cant be completed and will run the function again.  will run recursively until a dungeon that is able to be completed is made

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,5 +1,3 @@
-// window.addEventListener("load", buildDungeon);
-
 // ==========================
 // BUILDING OUT THE DUNGEON
 // ==========================
@@ -27,34 +25,45 @@ buildDungeon(
   TILE_HEIGHT,
   TILE_WIDTH
 );
-
-function buildDungeon(cHeight, cWidth, columns, rows, tHeight, tWidth) {
-  const game = document.getElementById("game");
-  const ctx = game.getContext("2d")
+  
+  function buildDungeon(cHeight, cWidth, columns, rows, tHeight, tWidth) {
+    const game = document.getElementById("game");
+    const ctx = game.getContext("2d")
     console.log(game)
     const COORDINATES = {};
-  // SET UP COORDINATE PLANE
-  columns.forEach((vX, x) => {
-    x = x * tWidth;
-    COORDINATES[x] = {};
+    // SET UP COORDINATE PLANE
+    columns.forEach((vX, x) => {
+      x = x * tWidth;
+      COORDINATES[x] = {};
       
-    rows.forEach((vY, y) => {
-      y = y * tHeight;
-      const cellChance = Math.random();
-      
-      COORDINATES[x][y] = cellChance <= WALKABLE_TILE_CHANCE ? 1 : 0
-      ctx.fillStyle = COORDINATES[x][y] === 1 ? "green" : "red";
-      ctx.fillRect(x, y, tWidth, tHeight)
+      rows.forEach((vY, y) => {
+        y = y * tHeight;
+        const cellChance = Math.random();
+        
+        COORDINATES[x][y] = cellChance <= WALKABLE_TILE_CHANCE ? 1 : 0
+        ctx.fillStyle = COORDINATES[x][y] === 1 ? "green" : "red";
+        ctx.fillRect(x, y, tWidth, tHeight)
+      });
     });
-  });
+    // console.log(COORDINATES);
+    
+    // console.log(COORDINATES[0])
+    // console.log(COORDINATES[1])
+    // console.log(COORDINATES[2])
+    // console.log(COORDINATES[3])
+    
+    //////////////////////////////////////////
+    // these next two functions, start and end,
+    // can go away once we have a system in
+    // place for picking a start and exit
+    ///////////////////////////////////////////
+    const start = (x=0,y=0) => COORDINATES[x][y] ? [x,y]:start(x+TILE_WIDTH, y)
+    const end = (x=0, y = 160) => COORDINATES[x][y] ? [x,y]:end(x, y-TILE_HEIGHT)
 
-  console.log(COORDINATES);
-
-  // console.log(COORDINATES[0])
-  // console.log(COORDINATES[1])
-  // console.log(COORDINATES[2])
-  // console.log(COORDINATES[3])
-
+    // checks to make sure the dungeon can be completed.
+    // if not build another one until you get one that can be
+    if(!checker(COORDINATES, start(), start(), end(), start(), 1, TILE_HEIGHT, [CANVAS_WIDTH - TILE_WIDTH, CANVAS_HEIGHT - TILE_HEIGHT]))
+      return buildDungeon(cHeight, cWidth, columns, rows, tHeight, tWidth);
   console.log("this is the dungeon");
 }
 
