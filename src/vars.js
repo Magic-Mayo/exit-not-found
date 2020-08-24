bb27ffde3495702ac775b1494787cfecd31fdff8
Sean <1996.sean.alexander@gmail.com>
2020-08-23 20:52:56 -0700

    created base js file for defining new variables

---
+++ src/vars.js
@@ -0,0 +1,24 @@
+
+// GET CANVAS ELEMENT FROM INDEX.HTML AND CREATE CONTEXT
+const game = document.getElementById("gameBG");
+const ctx = game.getContext("2d");
+
+
+
+/* ==================================
+CONSTANTS USED TO DEFINE THE DUNGEON CONSTRAINTS
+==================================== */
+const CANVAS_HEIGHT = 256;
+const CANVAS_WIDTH = 256;
+const NUMBER_OF_ROWS = 16;
+const NUMBER_OF_COLUMNS = 16;
+const ROWS = new Array(NUMBER_OF_ROWS).fill();
+const COLUMNS = new Array(NUMBER_OF_COLUMNS).fill();
+const TILE_HEIGHT = CANVAS_HEIGHT / NUMBER_OF_ROWS;
+const TILE_WIDTH = CANVAS_WIDTH / NUMBER_OF_ROWS;
+const WALKABLE_TILE_CHANCE = 0.8;
+/* ==================================
+==================================== */
+
+
+const COORDINATES = {};
