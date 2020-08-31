96a67023dfbd0253632fa9ff7b196f3e84f99053
Magic-Mayo <mike.mayoii@gmail.com>
2020-08-31 12:10:26 -0700

    moved endpoint and dir functions from dungeon. changed player movement function to incorporate 2d exit array and check if a certain exit was hit

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
 const rng = (n = 4) => Math.floor(Math.random() * n);
-const inverseCoords = ([x, y]) => !x ? [240, y] : !y ? [x, 240] : x == 241 ? [0, y] : [x, 0];
+const inverseCoords = ([x, y], max) => !x ? [max, y] : !y ? [x, max] : x > max ? [0, y] : [x, 0];
 const roundUp = (num) => {
 	const split = num.toString().split(".")[1];
 	return split
@@ -14,9 +14,9 @@ const generatePlayer = (coord, color, room, tileSize, exit) => {
 		handlePlayerMovement(e, room, tileSize, color, exit);
 
 	playerCoord = coord;
-	console.log(
-		`The player will start at ${playerCoord} and will have the color ${color}`
-	);
+	// console.log(
+	// 	`The player will start at ${playerCoord} and will have the color ${color}`
+	// );
 	ctx.fillStyle = room[coord[0]][coord[1]] && color;
 	ctx.fillRect(coord[0], coord[1], tileSize, tileSize);
 
@@ -40,16 +40,16 @@ const getEnemyCoordinate = (start, end, room) => {
 	const x = Math.floor(Math.random() * NUMBER_OF_ROWS) * TILE_HEIGHT;
 	const y = Math.floor(Math.random() * NUMBER_OF_ROWS) * TILE_HEIGHT;
 
-	return x !== start[0] &&
-		y !== start[1] &&
-		x !== end[0] &&
-		y !== end[1] &&
-		room[x][y]
+	return x != start[0] &&
+		y != start[1] &&
+		x != end[0] &&
+		y != end[1] &&
+		room[x]?.[y]
 		? [x, y]
 		: getEnemyCoordinate(start, end, room);
 };
 
-const handlePlayerMovement = (event, room, tileSize, color, exit) => {
+const handlePlayerMovement = (event, room, tileSize, color, exits) => {
 	if (
 		!(
 			event.code == "KeyW" ||
@@ -83,24 +83,26 @@ const handlePlayerMovement = (event, room, tileSize, color, exit) => {
 		playerCoord = [nextX, nextY];
 		enemyPosition.forEach((pos, i) => handleEnemyMovement(room, pos, i));
 	}
+  for(let i=0; i<exits.length;i++){
+    if (nextX == exits[i][0] && nextY == exits[i][1]) {
+      console.log("you win!");
+      window.removeEventListener("keypress", handleKeyPress);
 
-	if (nextX == exit[0] && nextY == exit[1]) {
-		console.log("you win!");
-		window.removeEventListener("keypress", handleKeyPress);
-
-		buildDungeon(
-			CANVAS_HEIGHT,
-			CANVAS_WIDTH,
-			COLUMNS,
-			ROWS,
-			TILE_HEIGHT,
-			TILE_WIDTH,
-			[exit[0], exit[1]]
-		);
-	}
+      return buildDungeon(
+        CANVAS_HEIGHT,
+        CANVAS_WIDTH,
+        COLUMNS,
+        ROWS,
+        TILE_HEIGHT,
+        TILE_WIDTH,
+        [exits[i][0], exits[i][1]]
+      );
+    }
+  }
 };
 
 const handleEnemyMovement = (room, [x, y], i) => {
+  if(x == undefined || y == undefined) return;
 	const surroundings = [
 		{
 			pos: "top",
@@ -122,19 +124,20 @@ const handleEnemyMovement = (room, [x, y], i) => {
 			coord: [x - TILE_WIDTH, y],
 			available: room[x - TILE_WIDTH]?.[y],
 		},
-	];
-	const availableSurroundings = surroundings.filter((c) => c.available);
+  ];
+  
+  const availableSurroundings = surroundings.filter((c) => c.available);
+
 	ctx.clearRect(x, y, TILE_WIDTH, TILE_HEIGHT);
 	enemyPosition[i] =
 		availableSurroundings[
 			Math.floor(Math.random() * availableSurroundings.length)
-		].coord;
-	const [newX, newY] = enemyPosition[i];
+		]?.coord;
+	const [newX, newY] = enemyPosition[i] ? enemyPosition[i] : [,];
 
 	ctx.fillStyle = room[newX][newY] && "#940404";
 	ctx.fillRect(newX, newY, TILE_WIDTH, TILE_HEIGHT);
 };
-
 
 const goFullScreen = () => {
   if (body.requestFullscreen) {
@@ -147,3 +150,34 @@ const goFullScreen = () => {
     body.msRequestFullscreen();
   }
 }
+
+// must not be in a corner
+// must not be on the same side as another exit or the start
+const generateRandomEndpoint = ([sX, sY], tile, max, exit) => {
+  const exitStart = exit.length > 0 ? [[sX,sY], ...exit] : [[sX, sY]]
+  const x = rng(tile) * tile;
+  const y = x == 0 || x == max ? rng(tile) * tile : rng() > 2 ? 0 : max;
+  console.log(x,y);
+  
+  const newExit = exitStart.some(([eX, eY]) => {
+    console.log(eX,eY);
+    if(((x == max || x == 0) && x == eX) || ((y == max || y == 0) && y == eY)) return true;
+    return false;
+  })
+  
+  if(!newExit) return [x,y];
+  return generateRandomEndpoint([sX, sY], tile, max, exitStart)  
+};
+
+const dir = ([sX, sY], tile, max) => {
+  if (sX == tile) {
+    if (sY > tile) return 0;
+    else return 1;
+  } else if (sY == tile) {
+    if (sX < max) return 1;
+    else return 2;
+  } else if (sX == max) {
+    if (sY < max) return 2;
+    else return 3;
+  }
+};
