91dbee7b39cca792bca34b37b9cfaa98d41e1870
Sean <1996.sean.alexander@gmail.com>
2020-08-31 19:18:49 -0700

    var name change (player.attack => player.attackStrength)

--- src/gameplay.js
+++ src/gameplay.js
@@ -10,7 +10,7 @@ _btn.addEventListener('click', e => {
 
     _healthpoints.innerHTML = player.hp
     _lvl.innerHTML = player.lvl
-    _attack.innerHTML = player.attack
+    _attack.innerHTML = player.attackStrength
     _defense.innerHTML = player.def
 
     
