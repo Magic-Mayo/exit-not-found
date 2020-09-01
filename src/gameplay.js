845725416f683a2fdca9c7c78dfb70f2a496ba62
Magic-Mayo <mike.mayoii@gmail.com>
2020-08-31 23:47:33 -0700

    commented event listener instead of moving functionality out of it

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,31 +1,40 @@
-_btn.addEventListener('click', e => {
-    // goFullScreen();
+////////// TO DO ///////////
+// uncomment listener
+////////// TO DO ///////////
+// _btn.addEventListener('click', e => {
+//     goFullScreen();
 
-    _container.classList.remove('invisible')
-    _landing.classList.add('invisible')
-    _btn.classList.add('invisible')
+//     _container.classList.remove('invisible')
+//     _landing.classList.add('invisible')
+//     _btn.classList.add('invisible')
 
+//     console.log(player);
+    
+    
     player = new Character("slimey",0 )
-    console.log(player);
-
     _healthpoints.innerHTML = player.hp
     _lvl.innerHTML = player.lvl
     _attack.innerHTML = player.attackStrength
     _defense.innerHTML = player.def
-
-    
-    
-  })
-      buildDungeon(
-          CANVAS_HEIGHT,
-          CANVAS_WIDTH,
-          COLUMNS,
-          ROWS,
-          TILE_HEIGHT,
-          TILE_WIDTH,
-          [0, 160]
+    _agility.innerHTML = player.agility
+    _speed.innerHTML = player.speed
+    buildDungeon(
+      CANVAS_HEIGHT,
+      CANVAS_WIDTH,
+      COLUMNS,
+      ROWS,
+      TILE_HEIGHT,
+      TILE_WIDTH,
+      [0, 160]
       );
-
+      
+      
+  // })
+      
+      ////////// TO DO ///////////
+      // uncomment listener
+      ////////// TO DO ///////////
+    
 // STATS
 // 1. healthpoints
 // 2. exp points
