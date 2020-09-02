bdd2d2e315dc72522b1f131edb0baa8950a75f1f
Sean <1996.sean.alexander@gmail.com>
2020-09-02 09:07:32 -0700

    revamped logic for locating click pos on canvas el. added logic for notifying user if they clicked a tile occupied by an enemy(s)

--- src/gameplay.js
+++ src/gameplay.js
@@ -1,59 +1,75 @@
 ////////// TO DO ///////////
 // uncomment listener
 ////////// TO DO ///////////
-_btnStart.addEventListener('click', e => {
-    goFullScreen();
+_btnStart.addEventListener("click", (e) => {
+	// goFullScreen();
 
-    _container.classList.remove('invisible')
-    _landing.classList.add('invisible')
-    _btnStart.classList.add('invisible')
+	_container.classList.remove("invisible");
+	_landing.classList.add("invisible");
+	_btnStart.classList.add("invisible");
 
-    console.log(player);
-    
-    
-    player = new Character("slimey",0 )
-    _healthpointsCurrent.innerHTML = player.hp
-    _healthpointsMax.innerHTML = player.hp
-    _playerLvl.innerHTML = player.lvl
-    _playerAttackStrength.innerHTML = player.attackStrength
-    _playerAttackSpeed.innerHTML = player.speed
-    _playerDefense.innerHTML = player.def
-    _playerAgility.innerHTML = player.agility
-    _playerFOV.innerHTML = player.fov
-    buildDungeon(
-      CANVAS_HEIGHT,
-      CANVAS_WIDTH,
-      COLUMNS,
-      ROWS,
-      TILE_HEIGHT,
-      TILE_WIDTH,
-      [0, 160]
-      );
-      
-      
-  })
+	console.log(player);
 
+	player = new Character("slimey", 0);
+	_healthpointsCurrent.innerHTML = player.hp;
+	_healthpointsMax.innerHTML = player.hp;
+	_playerLvl.innerHTML = player.lvl;
+	_playerAttackStrength.innerHTML = player.attackStrength;
+	_playerAttackSpeed.innerHTML = player.speed;
+	_playerDefense.innerHTML = player.def;
+	_playerAgility.innerHTML = player.agility;
+	_playerFOV.innerHTML = player.fov;
+	buildDungeon(
+		CANVAS_HEIGHT,
+		CANVAS_WIDTH,
+		COLUMNS,
+		ROWS,
+		TILE_HEIGHT,
+		TILE_WIDTH,
+		[0, 160]
+	);
+});
 
-_btnReset.addEventListener('click', e => {
-    const didExit = confirm("Are You Sure You Want To Quit? You're progress will not be saved")
-    didExit && location.reload()
-    
-})
-      
-      ////////// TO DO ///////////
-      // uncomment listener
-      ////////// TO DO ///////////
-    
+_btnReset.addEventListener("click", (e) => {
+	const didExit = confirm(
+		"Are You Sure You Want To Quit? You're progress will not be saved"
+	);
+	didExit && location.reload();
+});
+
+////////// TO DO ///////////
+// uncomment listener
+////////// TO DO ///////////
+
 // STATS
 // 1. healthpoints
 // 2. exp points
 // 3. dungeon lvl
 // 4. total steps walked
 
+game.addEventListener("click", (e) => {
+	const { top, left } = game.getBoundingClientRect();
+	const percentDiff = game.width / e.target.clientWidth;
 
-game.addEventListener('click', e => {
-    const percentDiff = (game.width / e.target.clientWidth);
-    const adjustedX = e.layerX * percentDiff
-    const adjustedY = e.layerY * percentDiff
-    console.log(`adjusted x: ${adjustedX}, adjusted y: ${adjustedY}`);
-})
+	const adjustedX = (e.clientX - left) * percentDiff;
+	const adjustedY = (e.clientY - top) * percentDiff;
+	console.log(`adjusted x: ${adjustedX}, adjusted y: ${adjustedY}`);
+	console.log(enemies);
+
+	// THIS SHOULD RETURN AN ARRAY OF ONE OR MORE ENEMIES THAT ARE IN THE CLICK LOCATION
+	enemies.forEach((enemy) =>
+		checkIfEnemyCoord([adjustedX, adjustedY], enemy.coords)
+	);
+});
+
+const checkIfEnemyCoord = ([clickX, clickY], [enX, enY]) => {
+
+	if (
+		clickX >= enX &&
+		clickY >= enY &&
+		clickX < enX + TILE_WIDTH &&
+		clickY < enY + TILE_WIDTH
+	) {
+        console.log('you clicked an enemy');
+	}
+};
