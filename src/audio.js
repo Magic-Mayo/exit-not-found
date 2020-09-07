3f08c2f11ded951591ee748f68ac682b78770680
Sean <1996.sean.alexander@gmail.com>
2020-09-07 13:12:15 -0700

    added zzfx sounds for player/enemy movement

--- src/audio.js
+++ src/audio.js
@@ -1,5 +1,17 @@
-const pMove = new Audio('./src/sounds/Move.wav')
-const eMove = new Audio('./src/sounds/enemyMove.wav')
+
+const pMove = [
+    zzfxG(...[,.25,144,,,.07,2,.1,-95.1,43.9,191,.1,.01,,,.1,.03,,.04]),
+    zzfxG(...[,.25,144,,,.07,2,.1,-95.1,43.9,191,.1,.01,,,.1,.03,,.04]),
+    zzfxG(...[,.25,144,,,.07,2,.1,-95.1,43.9,191,.1,.01,,,.1,.03,,.04]),
+    zzfxG(...[,.25,144,,,.07,2,.1,-95.1,43.9,191,.1,.01,,,.1,.03,,.04]),
+]; // playerMove
+
+const eMove = [
+    zzfxG(...[,.5,30,.03,.01,.07,2,.1,1,20,130,.1,.01,,5,.1,.03,.8,.03,1]), 
+    zzfxG(...[,.5,30,.03,.01,.07,2,.1,1,20,130,.1,.01,,5,.1,.03,.8,.03,1]), 
+    zzfxG(...[,.5,30,.03,.01,.07,2,.1,1,20,130,.1,.01,,5,.1,.03,.8,.03,1]), 
+    zzfxG(...[,.5,30,.03,.01,.07,2,.1,1,20,130,.1,.01,,5,.1,.03,.8,.03,1]), 
+]
 const levelUpSound = new Audio('./src/sounds/Powerup.wav')
 const magicDamageSound = new Audio('./src/sounds/Magic.wav')
 const meleeDamageSound = new Audio('./src/sounds/Melee.wav')
