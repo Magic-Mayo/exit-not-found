0b431499b0c42d01311b62ffb1c19f40ff7c5657
Magic-Mayo <mike.mayoii@gmail.com>
2020-09-07 00:59:45 -0700

    changed coordinate property seen to hasBeenSeen and added an inSight key. moved checking for exit into conditional itself instead of setting variables to the exit coords. removed painting canvas as it will be handled in paintCanvas function except to paint everything black at the start

--- src/dungeon.js
+++ src/dungeon.js
@@ -18,48 +18,40 @@ const buildDungeon = (
 
   // SET UP COORDINATE PLANE
 	columns.forEach((vX, x, col) => {
-    const newX = x;
+        const newX = x;
 		x = x * tWidth;
 		COORDINATES[x] = {};
 
-		rows.forEach((vY, y, row) => {
-      let eX, eY;
-      const newY = y;
-			y = y * tHeight;
-			const cellChance = Math.random();
-      const newStartIsHere = sX === x && sY === y;
-      exits.forEach(([a,b]) => {
-        if(a == x && b == y){
-          eX = a;
-          eY = b;
-        }
-      })
-      COORDINATES[x][y] = {
-        seen: false
-      };
+        rows.forEach((vY, y, row) => {
+            const start = sX == x && sY == y;
+            const newY = y;
+            y = y * tHeight;
+            const cellChance = Math.random();
+            const newStartIsHere = sX === x && sY === y;
 
-      if(eX == x){
-        COORDINATES[x][y].walkable = 1;
-        COORDINATES[x][y].exit = 1;
-        
-        ctx.fillStyle = getExitGradient(x,y);
-        ctx.fillRect(x,y,tWidth,tHeight)
-      } else if(newX == 0 || newY == 0 || y == xyMax || x == xyMax){
-        COORDINATES[x][y].walkable = sX == x && sY == y ? 1 : 0;
-        ctx.fillStyle = sX == x && sY == y ? colors.player : colors.unwalkable
-        ctx.fillRect(x,y,tWidth,tHeight)
-        
-      } else if(x <= xyMax && y <= xyMax){
-        COORDINATES[x][y].walkable =
-        cellChance <= WALKABLE_TILE_CHANCE || newStartIsHere ? 1 : 0;
-        COORDINATES[x][y].walkable ? null : COORDINATES[x][y].occupied = 1;
-        
-        ctx.fillStyle = COORDINATES[x][y].walkable === 0 ? colors.unwalkable : COORDINATES[x][y].seen ? colors.walkable.seen : colors.walkable.unseen;
-        ctx.fillRect(x, y, tWidth, tHeight);
-      }
+            COORDINATES[x][y] = {
+                hasBeenSeen: 0,
+                inSight: 0
+            };
 
-		});
-	});
+            if(exits.some(([a,b]) => a == x && b == y)){
+                console.log('exit')
+                COORDINATES[x][y].walkable = 1;
+                COORDINATES[x][y].exit = 1;
+                
+            } else if(newX == 0 || newY == 0 || y == xyMax || x == xyMax){
+                COORDINATES[x][y].walkable = sX == x && sY == y ? 1 : 0;
+                start ? COORDINATES[x][y].start = 1 : COORDINATES[x][y].border = 1;
+            
+            } else if(x < xyMax && y < xyMax){
+                COORDINATES[x][y].walkable =
+                cellChance <= WALKABLE_TILE_CHANCE || newStartIsHere ? 1 : 0;
+                COORDINATES[x][y].walkable ? null : COORDINATES[x][y].occupied = 1;
+            }
+            ctx.fillStyle = colors.unseenOrUnwalkable;
+            ctx.fillRect(x, y, tWidth, tHeight);
+        });
+    });
 
   
 	// checks to make sure the dungeon can be completed.
@@ -68,10 +60,10 @@ const buildDungeon = (
         buildDungeon(cHeight, cWidth, columns, rows, tHeight, tWidth, [pExitX,pExitY])
     );
   
-  generatePlayer([sX, sY], COORDINATES, tHeight);
-  generateEnemies(lvl, xyMax, COORDINATES);
+    generatePlayer([sX, sY], COORDINATES, tHeight);
+    generateEnemies(lvl, xyMax, COORDINATES);
 
-  _dungeon.innerHTML = dungeon++;
+    _dungeon.innerHTML = dungeon++;
 };
 
 
